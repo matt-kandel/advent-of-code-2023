687e33b93257c60977da8fc8eb0e05da754b1ddb
matt-kandel <mat.heiden@gmail.com>
2023-12-01 16:29:05 -0500

    stylistic updates

--- scripts/day_01.jl
+++ scripts/day_01.jl
@@ -1,13 +1,12 @@
 lines = readlines(".\\inputs\\day_01.txt");
 
 # Day One, Part A
-function process_line(line::String)
-    number_string = join([char for char ∈ line if char ∈ '1':'9'])
-    first_and_last_number = "$(number_string[1])$(number_string[end])"
-    return parse(Int, first_and_last_number)
+function decode(line::String)
+    digits = join([char for char ∈ line if char ∈ '1':'9'])
+    return parse(Int, "$(digits[1])$(digits[end])")
 end;
 
-println("Answer to 1a is $(sum(process_line.(lines)))");
+println("Answer to 1a: $(sum(decode.(lines)))");
 
 # Day One, Part B
 regex = r"(\d|one|two|three|four|five|six|seven|eight|nine)";
@@ -18,12 +17,12 @@ function as_digit(string)
                            "seven" => "7", "eight" => "8", "nine" => "9")
 end;
 
-function new_process_line(regex::Regex, line::String)
+function decode(regex::Regex, line::String)
     # Need overlap=true so that "twoneight" matches two, one, eight
-    matches = [match for match in eachmatch(regex, line, overlap=true)]
+    matches = eachmatch(regex, line, overlap=true) |> collect
     first_match, last_match = matches[1][1], matches[end][1]
     number_as_string = "$(as_digit(first_match))$(as_digit(last_match))"
     return parse(Int, number_as_string)
 end;
 
-println("Answer to 1b is $(sum(new_process_line.(regex, lines)))");
+println("Answer to 1b: $(sum(decode.(regex, lines)))");
